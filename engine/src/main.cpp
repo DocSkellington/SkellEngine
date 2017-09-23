834ae7606c2cd8154fcdbcb5e79de31eb3ba39e0
Gaëtan Staquet <gaetan.staquet@gmail.com>
2017-09-23 22:28:41 +0200

    Added SFML dependency

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -2,7 +2,24 @@
 #include <cmath>
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
+#include "config.h"
+
+using namespace sf;
 int main (int argc, char *argv[])
 {
+  std::cout << ENGINE_VERSION_MAJOR << '\n';
+  RenderWindow window (VideoMode(800, 600, 32), "TEST");
+
+  while (window.isOpen()) {
+    Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == Event::Closed)
+        window.close();
+    }
+
+    window.display();
+  }
   return 0;
 }
