09613c67919f378d2ccd650ebd084d907be6483d
Gaëtan Staquet <gaetan.staquet@gmail.com>
2019-08-27 14:24:16 +0200

    The Engine::run catches every exception

--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -37,39 +37,47 @@ namespace engine {
         float lastTime = 0;
         float FPS = 0;
 
-        while (m_context.window->isOpen()) {
-            m_context.inputHandler->clearInputs();
+        try {
+            while (m_context.window->isOpen()) {
+                m_context.inputHandler->clearInputs();
 
-            sf::Event event;
-            while (m_context.window->pollEvent(event)) {
-                m_context.stateManager->handleEvent(event);
-                m_context.inputHandler->proccess(event);
+                sf::Event event;
+                while (m_context.window->pollEvent(event)) {
+                    m_context.stateManager->handleEvent(event);
+                    m_context.inputHandler->proccess(event);
+                }
+
+                m_context.inputHandler->triggerEvents();
+
+                sf::Time elapsed = clock.restart();
+
+                m_context.stateManager->update(elapsed.asMicroseconds());
+
+                m_context.window->clear();
+
+                m_context.stateManager->draw(*m_context.window);
+
+                m_context.window->display();
+
+                m_context.stateManager->processSwitchToAndRemove();
+
+                float now = FPSClock.getElapsedTime().asSeconds();
+                // We use a moving average to smooth the FPS display
+                FPS = 0.6 * FPS + 0.4 * 1. / (now - lastTime);
+                // tmx::Logger::log(std::to_string(FPS));
+                lastTime = now;
+
+                // To avoid overflow
+                if (FPSClock.getElapsedTime().asSeconds() >= 100.f) {
+                    FPSClock.restart();
+                }
             }
-
-            m_context.inputHandler->triggerEvents();
-
-            sf::Time elapsed = clock.restart();
-
-            m_context.stateManager->update(elapsed.asMicroseconds());
-
-            m_context.window->clear();
-
-            m_context.stateManager->draw(*m_context.window);
-
-            m_context.window->display();
-
-            m_context.stateManager->processSwitchToAndRemove();
-
-            float now = FPSClock.getElapsedTime().asSeconds();
-            // We use a moving average to smooth the FPS display
-            FPS = 0.6 * FPS + 0.4 * 1. / (now - lastTime);
-            // tmx::Logger::log(std::to_string(FPS));
-            lastTime = now;
-
-            // To avoid overflow
-            if (FPSClock.getElapsedTime().asSeconds() >= 100.f) {
-                FPSClock.restart();
-            }
+        }
+        catch (const std::exception &e) {
+            m_context.logger.logError("Major error", e);
+        }
+        catch (...) {
+            m_context.logger.log("Unknown major error. Impossible to print the error message", LogType::Error);
         }
     }
 }
